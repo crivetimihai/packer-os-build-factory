865d9929b3e95bf4a356b4ce79090481d601f3af
Criveti Mihai <crivetimihai@gmail.com>
2019-06-27 18:29:38 +0100

    Vagrantfile updates

--- src/provisioners/rhel-8-base-install-ansible.sh
+++ src/provisioners/rhel-8-base-install-ansible.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --directory-prefix /tmp
+ANACONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+
+(cd /tmp && curl -O ${ANACONDA_URL})
 sh /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/anaconda3 > /tmp/anaconda3.log
 tail -5 /tmp/anaconda3.log
+export PATH="$HOME/anaconda3/bin:$PATH"
 echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> ~/.bashrc
-. ~/.bashrc
 
 # Upgrade pip and install ansible
 pip install --upgrade pip
